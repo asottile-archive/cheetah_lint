f9d372bf24bab751713feff421f451ba1ba4ab0b
Anthony Sottile <asottile@umich.edu>
2016-05-25 13:58:09 -0700

    Without py26 we don't need five.n()

--- cheetah_lint/flake.py
+++ cheetah_lint/flake.py
@@ -404,7 +404,7 @@ def flake(filename):
     file_contents = read_file(filename)
     flakes = get_flakes(file_contents)
     for lineno, msg in flakes:
-        print(five.n('{}:{} {}'.format(filename, lineno, msg)))
+        print('{}:{} {}'.format(filename, lineno, msg))
     return int(bool(flakes))
 
 
