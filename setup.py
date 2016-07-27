8f8df4247cfc8da4cd3e665c6575bc9398da8488
Anthony Sottile <asottile@umich.edu>
2016-07-26 18:00:39 -0700

    Temporarily require flake8<3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'aspy.refactor_imports>=0.2.1',
         'cached-property',
-        'flake8>=2.6.0',
+        'flake8>=2.6.0,<=2.999',
         'refactorlib[cheetah]>=0.13.0,<=0.13.999',
         'yelp-cheetah>=0.17.0,<=0.17.999',
     ],
